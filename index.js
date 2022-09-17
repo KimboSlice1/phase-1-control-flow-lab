548dab1d59b669b7cafcd12129189c08ca11832f
Kimber Herron <kh582013@gmail.com>
2022-09-17 13:28:38 -0400

    commit tree c0fca3195dd32a0ffc8326ae33bc47648a2bb039

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!';
+  }
+  {
+  if (distance >= 401 && distance <= 2000) {
+    return 'That will be twenty bucks.';
+  }
+   if (distance >= 2001 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (distance >= 2501) {
+    return 'No can do.'
+  }
+  console.log(scuberGreetingForFeet());
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  
+  function ternaryCheckCity(NYC) {
+    return (NYC ? 'Ok, sounds good.' : 'No go.');
+  }
+    console.log(ternaryCheckCity()); 
+    
+
+function switchOnCharmFromTip(amount){
+  switch (amount) {
+    case 'generous':
+       return 'Thank you so much.';
+       break;
+    case 'not as generous':
+        return 'Thank you.';
+        break;
+    default:
+      'Thanks for everything';
+      return 'Bye.'; }
 }
+    console.log(switchOnCharmFromTip());}}
